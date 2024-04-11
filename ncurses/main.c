e53c4e1d6cd99acbe1337dc1766f5aac4b4528b1
flow-state-15 <purcellgroup88@gmail.com>
2024-04-11 18:36:41 -0400

    prog 2

--- ncurses/main.c
+++ ncurses/main.c
@@ -43,45 +43,40 @@ int main(void) {
     refresh();
     wrefresh(w_out);
     wrefresh(w_in);
-    // printw prints to standard screen by default
-    // printw("hello screen!\n");
 
     char input_buffer[LEN_INPUT_BUFF] = {0};
     char **output_buffer = malloc((MAX_OUTPUT_LINES) * sizeof(char));
-
     size_t cursor_p = 0;
     int ch;
-    /*
-        TODO: we need an output buffer to keep line number analogous to cursor
-        pos and stores input buffer output in a char **(an array of strings) ex char[MAX_LINES][LEN_INPUT_BUFFER]
-    */
-    // 27 is char code for ESC key
-    // ncurses gives you macro for backspace key
-    int o_idx = 0;
-    int c_out_start = 2;
+    int o_idx = 0; // current idx of output buffer
+    int c_out_start = 2; // current line of window
     while((ch = wgetch(w_in)) != ESC_KEY){
         if(ch == '\n') {
             output_buffer[o_idx] = create_char_buffer(input_buffer);          
             memset(input_buffer, 0, LEN_INPUT_BUFF);
             mvwprintw(w_out, c_out_start++, 1, "%s", output_buffer[o_idx]);
             wrefresh(w_out);
-            if (o_idx++ == MAX_OUTPUT_LINES - 1) {
-                // reset output lines
+            if (o_idx == MAX_OUTPUT_LINES - 1) {
                 mvwprintw(w_out, c_out_start, 1, "%s", "max lines reached, resetting on next entry...");
                 wrefresh(w_out);
+            } else if (o_idx == MAX_OUTPUT_LINES) {
+                // reset output lines
                 o_idx = 0;
                 c_out_start = 2;
-                while (o_idx < MAX_OUTPUT_LINES + 1) {
+                while (o_idx < MAX_OUTPUT_LINES) {
                     if (output_buffer[o_idx] != NULL) memset(output_buffer[o_idx], 0, LEN_INPUT_BUFF);
                     wmove(w_out, c_out_start + o_idx, 1); wclrtoeol(w_out);
                     o_idx++;
                 }
+                wmove(w_out, c_out_start + o_idx, 1); wclrtoeol(w_out);
+                
                 output_buffer[0] = (char *) memcpy(output_buffer[0], input_buffer, LEN_INPUT_BUFF);
                 mvwprintw(w_out, c_out_start, 1, "%s", output_buffer[0]);
                 wrefresh(w_out);
-                o_idx = 0;
-                c_out_start++;
+                o_idx = 1;
+                c_out_start = 3;
             }
+            o_idx++;
             wmove(w_in, 1, 3);
             memset(input_buffer, 0, LEN_INPUT_BUFF);
             wclrtoeol(w_in);
